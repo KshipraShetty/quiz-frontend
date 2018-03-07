1436d0ada9be2b50406a03ca503b07203f0abb82
kshiprashetty <kshiprashetty@gmail.com>
2018-03-07 06:01:43 +0530

    initial boilerplate setup

---
+++ src/components/app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: 0,
+    };
+  }
+
+
+  render() {
+    return (
+      <div className="App" />
+
+    );
+  }
+}
+
+export default App;
